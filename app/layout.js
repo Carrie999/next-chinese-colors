d01ef341a74df96eb8b441d55b987e2ea9ca6d3e
carrie <771685809@qq.com>
2024-04-03 14:46:01 +0800

    feat: meta

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "中国传统色和日本传统色",
-  description: "中国传统色｜日本传统色｜传统色｜颜色",
+  title: "中国传统色和日本传统色,Chinese traditional colors and Japanese traditional colors",
+  description: "中国传统色｜日本传统色｜传统色｜颜色, Chinese Traditional Colors | Japanese Traditional Colors | Traditional Colors | Colors",
 };
 
 export default function RootLayout({ children }) {
